434c1a93e08d3f6d97ec9bfd57232b21a06f7e23
gondoh <gondoh@gufii.net>
2016-07-08 10:37:41 +0900

    説明サイトURL追加

--- config.php
+++ config.php
@@ -9,6 +9,6 @@
 $title = 'Google Maps API キー設定プラグイン';
 $description = 'Google Maps API キー設定が可能になります。キーは<a href="https://console.developers.google.com/" target="_blank">Googe APIs Console</a>にて取得してください。';
 $author = 'gondoh';
-$url = '';
+$url = 'http://blog.gufii.net/weblog/archives/49';
 $adminLink = array('plugin' => 'google_maps_api_key_pacher', 'controller' => 'google_maps_api_key_pacher', 'action' => 'index');
 $installMessage = 'Googe APIs Consoleにて払いだされたキーが必要になります。準備していない場合はインストール後にAPIキーを取得してください。';
